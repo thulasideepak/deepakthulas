48594e4d573bd64d990d64b1d8ba2a8dbaee96df
thulasideepak <35525691+thulasideepak@users.noreply.github.com>
2018-01-21 15:49:12 +0530

    Create EVEN.C
    to find the given number is odd or even

---
+++ EVEN.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{ 
+inta;
+clrscr();
+printf("\n enter the value of a:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("\n the given number is even");
+}
+else
+{
+printf("\n the given number is odd");
+}
+getch();
+}
